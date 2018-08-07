3b9abd028b5e21f1701bedbb20b1c6de17843b19
Francisco <fg_1@live.com>
2018-08-07 12:35:57 -0500

    Added more account statuses.

--- src/meta/accounts.js
+++ src/meta/accounts.js
@@ -2,6 +2,9 @@ const accountStatusMeta = {
   new: {id: 'new'},
   paymentSetup: {id: 'needsPayment'},
   feedSetup: {id: 'feedSetup'},
+  live: {id: 'live'},
   feedError: {id: 'feedError'},
   paymentError: {id: 'paymentError'},
 }
+
+module.exports = {accountStatusMeta}
--- src/meta/index.js
+++ src/meta/index.js
@@ -1,6 +1,6 @@
 const users = require('./users')
 const dealership = require('./dealership')
 const products = require('./products')
-const accountStatusMeta = require('./accounts')
+const accounts = require('./accounts')
 
-module.exports = {users, dealership, products}
+module.exports = {users, dealership, products, accounts}
