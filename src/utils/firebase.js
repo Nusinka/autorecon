b1de180dd4e006cac4e891c92aa6c57afe4b66ec
Logan Morrow <eloh@Sierra-iMac.local>
2018-08-13 22:12:32 -0500

    merging with .16

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,19 @@ function firebaseTimestampToMoment(timestamp) {
     return moment(timestamp.seconds * 1000)
 }
 
+
+function getDataFromQuerySnapshot (snapshot) {
+    const data = {}
+    snapshot.forEach(snapShot => {
+      const item = snapShot.data()
+      let id = snapShot.id
+      data[id] = item
+    })
+    return data
+  }
+
 module.exports = {
     firebaseTimestampToDate,
-    firebaseTimestampToMoment
+    firebaseTimestampToMoment,
+    getDataFromQuerySnapshot
 }
